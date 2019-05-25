f251d13df422451f9207566a489ea9d8125e548f
paulohgama <santosps1990@gmail.com>
2019-05-24 22:01:41 -0300

    Final da aula

--- src/Semana16/ArvoreDeDecisao.java
+++ src/Semana16/ArvoreDeDecisao.java
@@ -9,18 +9,25 @@ public class ArvoreDeDecisao {
 
     public static void main(String[] args) throws Exception {
         DataSource arff = new DataSource("data/contact-lenses.arff");
+        DataSource arff2 = new DataSource("data/iris.arff");
         //Instancia e reordena
         Instances contact = arff.getDataSet(4);
+        Instances iris = arff2.getDataSet(4);
         //Define indice da classe
         Id3 arvore = new Id3();
-        
-        arvore.buildClassifier(contact);
-        
         J48 arvoreJ48 = new J48();
-        
+        J48 arvoreirisJ48 = new J48();
+
+        arvore.buildClassifier(contact);
         arvoreJ48.buildClassifier(contact);
+        arvoreirisJ48.buildClassifier(iris);
         
+        System.out.println(arvore + "\n");
+        System.out.println("-----------------------------------------------");
         System.out.println(arvoreJ48);
-    }
+        System.out.println("-----------------------------------------------");
+        System.out.println(arvoreirisJ48);
+        
+}
     
 }
